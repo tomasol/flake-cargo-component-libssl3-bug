ac25a1344c0f0a01eed53b5e02bab0cf83cd98c5
Tomas Olvecky <tomas.olvecky@gmail.com>
2024-01-23 10:36:56 +0100

    Attempt to fix the issue using LD_LIBRARY_PATH

--- flake.nix
+++ flake.nix
@@ -25,12 +25,13 @@
         {
           devShells.default = pkgs.mkShell {
             inherit buildInputs nativeBuildInputs ;
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ]; # https://discourse.nixos.org/t/program-compiled-with-rust-cannot-find-libssl-so-3-at-runtime/27196/2
             shellHook = ''
               project_root="$(git rev-parse --show-toplevel 2>/dev/null)"
               export CARGO_INSTALL_ROOT="$project_root/.cargo"
               export PATH="$CARGO_INSTALL_ROOT/bin:$PATH"
-              cargo_packages="cargo-component@0.4.1"
-              cargo install --offline $cargo_packages 2>/dev/null || cargo install $cargo_packages
+              cargo_packages="cargo-component@0.6.0"
+              cargo install --offline $cargo_packages 2>/dev/null || cargo install --debug $cargo_packages
               '';
 
           };
